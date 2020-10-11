ab9567b74e5f96ff4f1963fdd325433fcb6c2fe6
Aszura <saderdan9@gmail.com>
2020-10-11 20:42:46 +0300

    Javascript code highlighting. Front and back styling To Be Completed

--- React/Prism.js
+++ React/Prism.js
@@ -1,9 +1,7 @@
-// const pathname = require("pathname");
 const Prism = require('prismjs');
 const copy = require('clipboardy');
 
 let code = copy.readSync();
-let html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
+let html = Prism.highlight(code, Prism.languages.jsx, 'jsx');
 html = `<div class="code_container"><pre><code>${html}</code></pre></div>`;
 copy.write(html);
-
